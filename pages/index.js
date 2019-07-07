366f573cda60d72f9be6dc564fa1484b56fd897e
Daniel Sandoval <daniel@desandoval.net>
2019-07-07 19:15:22 -0400

    start selecting things

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '../src/Link';
+import TextContent from '../src/TextContent';
 
 
 export default function Index() {
+  const [selection, setSelection] = useState("");
+  const mouseUpHandler = e => {
+    e.stopPropagation();
+    const domSelection = window.getSelection();
+    if (domSelection.focusNode.data) {
+      setSelection(domSelection.focusNode.data);
+    }
+    // setSelection(window.getSelection());
+  }
+
+  useEffect(() => {
+    window.onmouseup = mouseUpHandler;
+  })
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
@@ -15,6 +30,13 @@ export default function Index() {
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
+        <Typography>
+          Currently selected:<br/>
+          {selection}
+          <br/>
+          <br/>
+        </Typography>
+        <TextContent />
       </Box>
     </Container>
   );
