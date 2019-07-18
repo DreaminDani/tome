13a13ab09695feb03adccfcdcdc905356d8aa368
Daniel Sandoval <daniel@desandoval.net>
2019-07-17 21:09:47 -0400

    add passport for auth. cannot get isAuthorized to be true

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,17 @@ import theme from '../src/theme';
 import Menu from '../src/molecule/menu';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    if (ctx.req && ctx.req.session.passport) {
+      pageProps.user = ctx.req.session.passport.user;
+    }
+    return { pageProps };
+  }
+
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import fetch from "isomorphic-fetch";
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '../src/Link';
 
 
-export default function Index() {
+function Index(props) {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
@@ -19,7 +20,32 @@ export default function Index() {
         <Link href="/artifact" color="primary">
           Go to an artifact page
         </Link>
+        {props.thoughts &&
+          props.thoughts.map(thought => (
+              <Typography key={thought._id}>
+                {thought.message} | {thought.author}
+              </Typography>
+          ))}
+        {!props.thoughts && <Typography>Loading...</Typography>}
       </Box>
     </Container>
   );
 }
+
+Index.getInitialProps = async ({ req }) => {
+  const baseURL = req ? `${req.protocol}://${req.get("Host")}` : "";
+  const res = await fetch(`${baseURL}/api/thoughts`, {
+    credentials: 'include'  
+  });
+  if (res.status === 200) {
+    return {
+      thoughts: await res.json()
+    };
+  } else {
+    return {
+      thoughts: ["error"]
+    }
+  }  
+};
+
+export default Index;
