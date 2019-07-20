8b65b26e163785643ee41ba36aaaddf8fe4831c7
Daniel Sandoval <daniel@desandoval.net>
2019-07-20 10:39:52 -0400

    intentional failure

--- src/organism/artifact/artifact.test.js
+++ src/organism/artifact/artifact.test.js
@@ -44,7 +44,7 @@ describe('Artifact responds to mouse events', () => {
     expect(window.getSelection).toHaveBeenCalled();
 
     const domSelection = window.getSelection.mock.results[0].value;
-    expect(domSelection.removeAllRanges).toHaveBeenCalled();
+    expect(domSelection.removeAllRanges).not.toHaveBeenCalled();
   })
 
   it('does not update the selection, if nothing is clicked', () => {
