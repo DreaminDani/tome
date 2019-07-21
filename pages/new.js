85f94faced8950f5944c34445cf54a80adb0db63
Daniel Sandoval <daniel@desandoval.net>
2019-07-21 11:27:00 -0400

    all inputs on page

--- pages/new.js
+++ pages/new.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
-import { Box, Grid, TextField, makeStyles, Typography } from '@material-ui/core';
+import { Box, Grid, ButtonGroup, Button, TextField, makeStyles, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '0 16px',
+  },
   title: {
     maxWidth: 'calc(100% - 120px)', //header width
   },
@@ -9,7 +12,28 @@ const useStyles = makeStyles({
     fontWeight: 'bold',
     marginBottom: 8,
   },
-})
+  settings: {
+    [theme.breakpoints.down('xs')]: {
+      background: '#ffffff',
+      zIndex: 5,
+      boxShadow: theme.shadows[2],
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      marginLeft: -8,
+    }
+  },
+  actions: {
+    [theme.breakpoints.down('xs')]: {
+      float: 'right'
+    },
+    marginTop: 24,
+    marginBottom: 24
+  },
+  button: {
+    marginRight: 8,
+  }
+}));
 
 function New(props) {
   const classes = useStyles();
@@ -31,15 +55,16 @@ function New(props) {
   };
 
   return (
-    <div>
+    <div className={classes.root}>
       <Typography variant="h1" className={classes.title}>
           New Artifact
-        </Typography>
+      </Typography>
       <Grid
         container
         direction="row"
         justify="flex-start"
         alignItems="stretch"
+        spacing={2}
       >
         <Grid item xs={12} sm={8}>
           <TextField
@@ -52,7 +77,7 @@ function New(props) {
             variant="outlined"
             fullWidth
           />
-          <Box height="100%" maxHeight="50vh" width="100%" id="body-border">
+          <Box height="100%" maxHeight="40vh" width="100%" id="body-border">
             <TextField
               id="outlined-body"
               label="Body"
@@ -65,6 +90,22 @@ function New(props) {
               fullWidth
             />
           </Box>
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.settings}>
+          <div className={classes.actions}>
+            <Button className={classes.button}>Discard</Button>
+            <Button variant="contained" color="primary" className={classes.button}>
+              Save
+            </Button>
+          </div>
+          <Typography variant="h4" component="h2" gutterBottom>
+            Font type
+          </Typography>
+          <ButtonGroup aria-label="Small outlined button group">
+            <Button>Serif</Button>
+            <Button>Sans</Button>
+            <Button>Mono</Button>
+          </ButtonGroup>
         </Grid>
       </Grid>
     </div>
